edbae4f550f47f0c91cf1ccf44e7c72eb6746a45
coderOM1 <92622542+coderOM1@users.noreply.github.com>
2023-11-27 19:32:32 +0530

    AddingAuthorizationLayer

--- billingWebAPI/billingWebAPI/Program.cs
+++ billingWebAPI/billingWebAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using billingWebAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,7 +27,45 @@ builder.Services.AddDbContext<billingDBContext>(options => options.UseSqlServer(
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(sw => 
+
+sw.SwaggerDoc("v1",
+new OpenApiInfo { Title = "billingSw Api", Version = "1.0"}));
+
+builder.Services.AddSwaggerGen(s => 
+
+s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+{
+    In = ParameterLocation.Header,
+    Description = "INSERT JWT TOKEN",
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    BearerFormat = "JWT",
+    Scheme = "bearer"
+}));
+
+builder.Services.AddSwaggerGen(w =>
+
+w.AddSecurityRequirement(
+    
+    new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    }
+
+    )
+
+);
 
 var app = builder.Build();
 
